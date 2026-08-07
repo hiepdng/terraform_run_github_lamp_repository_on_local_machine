3b1b95ae74dbf0b493065e032a8e4c40e637039a
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-08-07 15:12:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 1.0.0"
-  #backend "local" {
-  #  path = "./terraform.tfstate"
-  #}
+  backend "local" {
+    path = "/home/temp/z/b/actions-runner/terraform.tfstate"
+  }
   required_providers {
     null = {
       source  = "hashicorp/null"
