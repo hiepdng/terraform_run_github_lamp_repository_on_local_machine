2d01c520df81cf2e75a9b795c7964a0277e662a5
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-08-06 21:21:15 -0700

    Enhance destroy command with env loading and cleanup
    Added a provisioner to load environment variables from a .env file and clean up Docker resources during terraform destroy.

--- main.tf
+++ main.tf
@@ -30,10 +30,27 @@ resource "null_resource" "lamp_stack" {
   provisioner "local-exec" {
     when    = destroy
     command = <<EOT
+      # Load .env file variables
+      if [ -f .env ]; then
+        while IFS= read -r line || [ -n "$line" ]; do
+          # Skip comments and empty lines
+          [[ "$line" =~ ^#.*$ ]] || [ -z "$line" ] && continue
+          echo "$line" >> $GITHUB_ENV
+        done < .env
+      else
+        echo ".env file not found"
+      fi
+      # Remove contrainers
       if [ -d "docker_build_DHI_LAMP_Project" ]; then
         cd docker_build_DHI_LAMP_Project
         docker compose down || docker-compose down
       fi
+      # Remove images
+      docker rmi "${PROJECT_NAME}-${HTTPDVERSION}" || true
+      docker rmi "${PROJECT_NAME}-${MYSQLVERSION}" || true
+      docker rmi "${PROJECT_NAME}-${PHPVERSION}"  || true
+      # Remove docker network
+      docker network ls --filter name=lamp_network -q |xargs -r docker network rm
     EOT
   }
 }
