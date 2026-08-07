db10a8c7959bb8eb2318412a2984928b70b05930
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-08-06 17:41:28 -0700

    Add Docker Hub username and token variables
    Added variables for Docker Hub credentials and GitHub Actions secrets.

--- main.tf
+++ main.tf
@@ -8,6 +8,28 @@ terraform {
   }
 }
 
+variable "dockerhub_username" {
+  type        = string
+  description = "Docker ID or Organization username for dhi.io registry"
+}
+
+variable "dockerhub_token" {
+  type        = string
+  description = "Docker Personal Access Token (PAT) for dhi.io authentication"
+  sensitive   = true
+}
+
+# Set GitHub Actions Secrets for dhi.io login
+resource "github_actions_secret" "dockerhub_username" {
+  secret_name     = "DOCKERHUB_USERNAME"
+  plaintext_value = var.dockerhub_username
+}
+
+resource "github_actions_secret" "dockerhub_token" {
+  secret_name     = "DOCKERHUB_TOKEN"
+  plaintext_value = var.dockerhub_token
+}
+
 resource "null_resource" "lamp_stack" {
   # Trigger re-execution if the repository URL changes
   triggers = {
